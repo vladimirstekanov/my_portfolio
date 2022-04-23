99021113ead8da556313c3c4d358125d7742fc7f
Vova <volodynarabote@yandex.ru>
2022-04-23 16:41:46 +0300

    final

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -2,13 +2,13 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-// $text= $_POST['text'];
+$message = $_POST['message'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
@@ -32,8 +32,8 @@ $mail->Subject = 'Данные';
 $mail->Body    = '
 		Пользователь оставил данные <br> 
 	Имя: ' . $name . ' <br>
-	E-mail: ' . $email . '' <br>
-    // Текст: ' . $name . ';
+	E-mail: ' . $email . ' <br>
+    Сообщение: ' . $message . '';
 
 if(!$mail->send()) {
     return false;
